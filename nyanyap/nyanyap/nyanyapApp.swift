a7e3b5b62fc41b02b888df016a7332747debfda0
Kazuyoshi Akaishi <k.akaishi@kazuyoshi_akaishi@mail.toyota.co.jp>
2023-05-16 19:01:38 +0900

    xcode init

---
+++ nyanyap/nyanyap/nyanyapApp.swift
@@ -0,0 +1,17 @@
+//
+//  nyanyapApp.swift
+//  nyanyap
+//
+//  Created by user on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct nyanyapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
